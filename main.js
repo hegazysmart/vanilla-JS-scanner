1e1db2eda580df7a591fe3a5f7da30e301d0635b
hegazy <hegazysmart@github.com>
2023-11-06 16:08:29 +0200

    add result

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function onScanSuccess(decodedText, decodedResult) {
   console.log(`Code scanned = ${decodedText}`, decodedResult);
+  document.getElementById("result").innerText = decodedResult
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
 "qr-reader", { fps: 10, qrbox: 250 });
