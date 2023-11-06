b5f65d4ad8f8212ade83fbcfdb50980d544d696d
hegazy <hegazysmart@github.com>
2023-11-06 15:59:28 +0200

    demo

--- main.js
+++ main.js
@@ -1,32 +1,7 @@
-const html5QrCode = new Html5Qrcode("reader");
-const config = { fps: 10, qrbox: 250 };
-let scanning = false;
-
-document.getElementById('start-button').addEventListener('click', function() {
-    if (scanning) return;
-    html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess, onScanFailure)
-    .catch(err => {
-        console.error(`Unable to start scanning, error: ${err}`);
-    });
-    scanning = true;
-});
-
-document.getElementById('stop-button').addEventListener('click', function() {
-    if (!scanning) return;
-    html5QrCode.stop().then(() => {
-        console.log("Scanning stopped");
-    }).catch(err => {
-        console.error(`Unable to stop scanning, error: ${err}`);
-    });
-    scanning = false;
-});
-
 function onScanSuccess(decodedText, decodedResult) {
-    document.getElementById('result').textContent = `Scanned result: ${decodedText}`;
-    console.log(`Code matched = ${decodedText}`, decodedResult);
+  console.log(`Code scanned = ${decodedText}`, decodedResult);
 }
+var html5QrcodeScanner = new Html5QrcodeScanner(
+"qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
 
-function onScanFailure(error) {
-    // Optionally handle scan failure, if you want to display a message to the user, for example
-    console.warn(`Code scan error = ${error}`);
-}
